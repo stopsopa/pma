96bddcd7408b0dbc8699574cf0e3a5d1e9722961
szymon <szymon.dzialowski@absolvent.pl>
2015-04-02 12:02:36 +0200

    u

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+DIR="pma"
+GET="$(wget -help &> /dev/null && echo "wget" || echo "curl -O")";
+T="$(date +%Y-%m-%d-%H-%M-%S)"
+
+if [ -e $DIR ]; then
+    echo -e "\e[31mJest już katalog '$DIR'\e[m"
+    exit 1
+fi
+
+mkdir -p $DIR
+
+if [ -e $DIR ]; then
+
+    $GET https://github.com/stopsopa/pma/archive/master.tar.gz?$T && mv master.tar.gz* master.tar.gz
+    
+    tar -zxvf master.tar.gz
+    cd stopsopa-pma
+    ls -la
+    pwd
+
+    exit 0
+else
+    echo -e "\e[31mNie można utworzyć katalogu '$DIR' brak uprawnień\e[m";
+    exit 1
+fi
